a1b1d3bcfd926bc4adc78ed3a05106822dd4adf9
Nora <nora.ekramy11@gmail.com>
2023-08-20 03:38:06 -0700

    _printf

---
+++ test_Nora/_printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - prints the input according to a format
+ * @format : the formating string 
+ *
+ * Return : number of printed chars
+ */
+int _printf(const char *format, ...);
+{
+	return 0;
+}
