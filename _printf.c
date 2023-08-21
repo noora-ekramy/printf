0a3ff353b6966728a1dfba91dc5ed22af0b48b65
malakehab2003 <lokaehab2003@gmail.com>
2023-08-21 13:00:29 +0000

    move files out

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+#define buffer 1024
+
+/**
+ * _printf - print any thing
+ *
+ * @format: the string to print
+ *
+ * Return: number of printed char
+*/
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0, i = 0;
+
+	va_start(args, format);
+	if (format == NULL)
+		format = "";
+	for (; format[i] != 0; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				count += print_char(va_arg(args, char));
+			else if (format[i] == 's')
+				count += print_string(va_arg(args, char *));
+			else if (format[i] == '%')
+				count += print_percent(void);
+			else if (format[i] == 'i' || format[i] == 'd')
+				count += print_int(va_arg(args, int));
+			else if (format[i] == 'b')
+				count += print_binary(va_arg(args, int));
+			else if (format[i] == 'u')
+				count += print_unsigned(va_arg(args, unsigned int));
+			else if (format[i] == 'o')
+				count += print_octal(va_arg(args, int));
+			else if (format[i] == 'x')
+				count += print_hex(va_arg(args, int));
+			else if (format[i] == 'X')
+				count += print_capital_hex(va_arg(args, int));
+
+			else
+			{
+				i--;
+				count += print_percent(void);
+			}
+		}
+		va_end(args);
+		return (++count);
+	}
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_number(int n);
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(char *s);
+int print_percent(void);
+int print_int(int i);
+int print_binary(int i);
+void print_unsigned_number(int n);
+int print_unsigned(int i);
+int print_octal(int i);
+int print_hex(int i);
+int print_capital_hex(int i);
+
+#endif
