47fc812b0f739c4cc7fbe13381175cb3f4203d00
malakehab2003 <lokaehab2003@gmail.com>
2023-08-21 14:59:37 +0000

    fix _printf function

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void print_number(int n);
 int _printf(const char *format, ...);
