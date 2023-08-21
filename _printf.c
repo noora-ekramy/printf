6c56716f6f65c87a9a5301e3ae7a9a364bc3729e
malakehab2003 <lokaehab2003@gmail.com>
2023-08-21 17:47:51 +0000

    fix _printf function

--- _printf.c
+++ _printf.c
@@ -2,7 +2,22 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
 
+int _putchar(char c);
+void print_number(int n);
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(const char *s);
+int print_percent(void);
+int print_int(int i);
+int print_binary(int i);
+void print_unsigned_number(unsigned int n);
+int print_unsigned(unsigned int i);
+int print_octal(int i);
+int print_hex(int i);
+int print_capital_hex(int i);
 
 
 /**
@@ -17,7 +32,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, i = 0;
+	unsigned int count = 0, i = 0;
 
 	va_start(args, format);
 	if (format == NULL)
@@ -60,4 +75,169 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+int print_binary(int i)
+{
+	int count = 0, binary = 0, tens = 1;
+
+	while (i > 0)
+	{
+		binary += (i % 2) * tens;
+		i /= 2;
+		tens *= 10;
+		count++;
+	}
+	print_unsigned_number(binary);
+	return (count);
+}
+
+
+int print_capital_hex(int i)
+{
+	int count = 0, division = 0, iterate = 0;
+	char hex[50];
+
+	while (i > 0)
+	{
+		division = i % 16;
+		if (division < 10)
+			hex[iterate] = '0' + division;
+		else
+			hex[iterate] = 'A' + division - 10;
+		i /= 16;
+		count++;
+		iterate++;
+	}
+	iterate--;
+	while (hex[iterate] != 0)
+	{
+		_putchar(hex[iterate]);
+		iterate--;
+	}
+	return (count);
+}
+
+int print_char(char c)
+{
+	_putchar(c);
+	return (1);
+}
+
+int print_hex(int i)
+{
+	int count = 0, division, iterate = 0;
+	char hex[50];
+
+	while (i > 0)
+	{
+		division = i % 16;
+		if (division < 10)
+			hex[iterate] = '0' + division;
+		else
+			hex[iterate] = 'a' + division - 10;
+		i /= 16;
+		count++;
+		iterate++;
+	}
+	iterate--;
+	while (hex[iterate] != 0)
+	{
+		_putchar(hex[iterate]);
+		iterate--;
+	}
+	return (count);
+}
+
+int print_int(int i)
+{
+	int count = 0;
+
+	print_number(i);
+	while (i > 0)
+	{
+		count++;
+		i /= 10;
+	}
+	return (count);
+}
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if (num >= 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar('0' + num % 10);
+}
+
+int print_octal(int i)
+{
+	int octal = 0, tens = 1, count = 0;
+
+	while (i > 0)
+	{
+		octal += (i % 10) * tens;
+		i /= 8;
+		tens *= 10;
+		count++;
+	}
+	print_number(octal);
+	return (count);
+}
+
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
+
+int print_string(const char *s)
+{
+	int i = 0, count = 0;
+
+	for (; s[i] != 0; i++)
+	{
+		_putchar(s[i]);
+		count++;
+	}
+	return (count);
+}
+
+int print_unsigned(unsigned int i)
+{
+	unsigned int count = 0;
+
+	print_unsigned_number(i);
+	while (i > 0)
+	{
+		count++;
+		i /= 10;
+	}
+	return (count);
+}
+
+void print_unsigned_number(unsigned int n)
+{
+	unsigned int num = n;
+
+	if (num >= 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar('0' + num % 10);
 }
