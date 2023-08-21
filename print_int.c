3d163b20a6657a464dced32983b9c58281248937
Nora Ekramy <nora.ekramy11@gmail.com>
2023-08-22 01:31:06 +0300

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -4,27 +4,42 @@
  * @arg: argument to print
  * Return: number of characters printed
  */
-int print_int(va_list arg)
-{
-        int n;
-        int len;
+int print_int(va_list arg) {
+    int n = va_arg(arg, int);
+    int num, last = n % 10, digit, exp = 1;
+    int i = 1;
 
-        n = va_arg(arg, int);
-        len=0;
-        if(n < 0)
-        {
-                n = -n;
-                _putchar('-');
-                len++;
-        }
-        if(n == 0)
-        {
-                _putchar('0');
-                len++;
-        }
-        len += print_number(n, 0);
+    n = n / 10;
+    num = n;
 
-        return (len);
+    if (last < 0) {
+        _putchar('-');
+        num = -num;
+        n = -n;
+        last = -last;
+        i++;
+    }
+
+    if (num > 0) {
+        while (num / 10 != 0) {
+            exp = exp * 10;
+            num = num / 10;
+        }
+
+        num = n;
+
+        while (exp > 0) {
+            digit = num / exp;
+            _putchar(digit + '0');
+            num = num - (digit * exp);
+            exp = exp / 10;
+            i++;
+        }
+    }
+
+    _putchar(last + '0');
+
+    return i;
 }
 /**
  * print_number - prints integer
