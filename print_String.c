59af9738ff24881685ba570b3482615f308e082e
malakehab2003 <lokaehab2003@gmail.com>
2023-08-22 22:55:23 +0000

    try print_String

--- print_String.c
+++ print_String.c
@@ -20,7 +20,7 @@ int print_String(va_list args)
 		{
 			_putchar('\\');
 			_putchar('x');
-			count += print_hexadecimal(str[itr]);
+			count += print_two_hexadecimal(str[itr]);
 			count += 2;
 		}
 		else
@@ -30,4 +30,46 @@ int print_String(va_list args)
 		}
 	}
 	return (count);
+}
+
+/**
+ * print_two_hexadecimal - print two hexadecimal numbers
+ *
+ * Return: always 2
+ *
+ * @i: the number to print
+*/
+
+int print_two_hexadecimal(unsigned int i)
+{
+	int division, iterate = 0;
+	char hex[10];
+
+	if (i == 0)
+	{
+		_putchar('0');
+		_putchar('0');
+		return (2);
+	}
+	while (i > 0)
+	{
+		division = i % 16;
+		if (division < 10)
+			hex[iterate] = '0' + division;
+		else
+			hex[iterate] = 'A' + division - 10;
+		i /= 16;
+		iterate++;
+	}
+	iterate--;
+	if (iterate == 0)
+	{
+		_putchar('0');
+	}
+	while (iterate >= 0)
+	{
+		_putchar(hex[iterate]);
+		iterate--;
+	}
+	return (2);
 }
